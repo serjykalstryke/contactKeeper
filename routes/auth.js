bba57ae51837ad5d4eed0d27c97046febf750705
David Stinnett <davidstinnett@icloud.com>
2021-01-23 08:46:27 -0500

    mongodb is running and routes are working, adding react

--- routes/auth.js
+++ routes/auth.js
@@ -3,13 +3,21 @@ const router = express.Router();
 const bcrypt = require("bcryptjs");
 const jwt = require("jsonwebtoken");
 const config = require("config");
+const auth = require("../middleware/auth");
 const { check, validationResult } = require("express-validator/check");
+const User = require("../models/User");
 
 //@route    GET api/auth
 //@desc     Get logged in user
 //@access   Private
-router.get("/", (req, res) => {
-	res.send("Get logged in user");
+router.get("/", auth, async (req, res) => {
+	try {
+		const user = await User.findById(req.user.id).select("-password");
+		res.json(user);
+	} catch (error) {
+		console.error(err.message);
+		res.status(500).send("Server error");
+	}
 });
 
 //@route    POST api/auth
@@ -21,8 +29,48 @@ router.post(
 		check("email", "Please include a valid email").isEmail(),
 		check("password", "Password is required").exists(),
 	],
-	(req, res) => {
-		res.send("Log in user");
+	async (req, res) => {
+		const errors = validationResult(req);
+		if (!errors.isEmpty()) {
+			return res.status(400).json({ errors: errors.array() });
+		}
+
+		const { email, password } = req.body;
+
+		try {
+			let user = await User.findOne({ email });
+
+			if (!user) {
+				return res.status(400).json({ msg: "Invalid Credentials" });
+			}
+
+			const isMatch = await bcrypt.compare(password, user.password);
+
+			if (!isMatch) {
+				return res.status(400).json({ msg: "Invalid Credentials" });
+			}
+
+			const payload = {
+				user: {
+					id: user.id,
+				},
+			};
+
+			jwt.sign(
+				payload,
+				config.get("jwtSecret"),
+				{
+					expiresIn: 3600,
+				},
+				(err, token) => {
+					if (err) throw err;
+					res.json({ token });
+				}
+			);
+		} catch (error) {
+			console.error(err.message);
+			res.status(500).send("Server Error");
+		}
 	}
 );
 
